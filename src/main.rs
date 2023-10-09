3798bbae5aa85cbe613fc20bae52be7c6a51b5c1
Novar <xuwab77@gmail.com>
2023-10-09 12:28:16 +0800

    Small Example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    // println! is a macro that prints text to the console
+    //println!("Hello, world!");
+    //print!("Test print!");
 
-    print!("Test print!")
+    //simple example
+    let mut x: i32 = 6; //mutable variable binding
+    print!("{x}"); //Macro for printing, like println! but without newline
+    while x != 1 {
+        //No parenthesis around expression
+        if x % 2 == 0 {
+            x = x / 2;
+        } else {
+            x = 3 * x + 1;
+        }
+        print!(" -> {x}");
+    }
+
+    println!();
 }
