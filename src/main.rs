4c516e7223f9b74c456aa0ef490f5f229c51259d
Novar <xuwab77@gmail.com>
2023-10-09 19:04:24 +0800

    References

--- src/main.rs
+++ src/main.rs
@@ -68,4 +68,37 @@ fn main() {
     // println!("The value of y is: {}", _y);
     // println!("The value of y is: {}", tup.1); //accessing tuple element
 
+    //References and borrowing
+    // let mut x: i32 = 10;
+    // let ref_x = &mut x;
+    // *ref_x = 20;
+    // println!("x: {x}");
+    // println!("ref_x: {ref_x}");
+    // let mut x: i32 = 10;
+    // let ref_x = &mut x;
+    // *ref_x = 20;
+    // // println!("x: {}", x);
+    // println!("ref_x: {}", ref_x);
+
+    //error: borrowed value does not live long enough
+    // let ref_x: &i32;
+    // {
+    //     let xx: i32 = 10;
+    //     ref_x = &xx;
+    // }
+    // println!("ref_x: {ref_x}");
+
+    // let ref_x: &i32;
+    // {
+    //     let xx: i32 = 10;
+    //     ref_x = &xx;
+    //     println!("ref_x: {}", ref_x);
+    // }
+
+    let xx: i32 = 10;
+    {   
+        let ref_x: &i32;
+        ref_x = &xx;
+        println!("ref_x: {}", ref_x);
+    } 
 }
