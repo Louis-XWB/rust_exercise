d85afccd85d2faec8718dd0e8b684ba921018778
Novar <xuwab77@gmail.com>
2023-10-17 02:34:27 +0800

    enum & match

--- src/main.rs
+++ src/main.rs
@@ -193,94 +193,313 @@ fn main() {
 
     // println!("is_child: {}", is_child(19));
 
-    struct User {
-        active: bool,
-        username: String,
-        email: String,
-        sign_in_count: u64,
+    // struct User {
+    //     active: bool,
+    //     username: String,
+    //     email: String,
+    //     sign_in_count: u64,
+    // }
+
+    // let user1 = User {
+    //     active: true,
+    //     username: String::from("user1"),
+    //     email: String::from("email@email.com"),
+    //     sign_in_count: 1,
+    // };
+
+    // let active = user1.active;
+    // let username = user1.username;
+    // let email = user1.email;
+    // let sign_in_count = user1.sign_in_count;
+    // // user1.active = false;
+
+    // let username = String::from("test");
+    // let mut user2 = User {
+    //     active: user1.active,
+    //     username,
+    //     email: String::from("email2@gmail.com"),
+    //     sign_in_count: 1,
+    // };
+
+    // user2.active = false;
+
+    // let user3 = User {
+    //     active: false,
+    //     ..user2
+    // };
+
+    // let customWidth = 10;
+
+    // let mut rectangle = Rectangle {
+    //     width: dbg!(10 * customWidth),
+    //     height: 20,
+    // };
+
+    // println!("area: {}", area(&rectangle));
+    // println!("rectangle: {:?}", rectangle);
+
+    // dbg!(&rectangle);
+
+    // println!("area: {}", rectangle.area());
+    // rectangle.inc_width(200);
+    // println!("area: {}", rectangle.area());
+
+    // let square = Rectangle::square(10);
+    // println!("square: {:?}", square);
+
+    // let isRightWidth = Rectangle::isRightWidth(10);
+    // println!("isRightWidth: {}", isRightWidth);
+
+    //enum
+    #[derive(Debug)]
+    enum IpAddrKind {
+        V4,
+        V6,
     }
 
-    let user1 = User {
-        active: true,
-        username: String::from("user1"),
-        email: String::from("email@email.com"),
-        sign_in_count: 1,
-    };
+    let ipV4 = IpAddrKind::V4;
+    let ipV6 = IpAddrKind::V6;
 
-    let active = user1.active;
-    let username = user1.username;
-    let email = user1.email;
-    let sign_in_count = user1.sign_in_count;
-    // user1.active = false;
+    println!("ipV4: {:?}", ipV4);
+    println!("ipV6: {:?}", ipV6);
 
-    let username = String::from("test");
-    let mut user2 = User {
-        active: user1.active,
-        username,
-        email: String::from("email2@gmail.com"),
-        sign_in_count: 1,
-    };
+    fn router(ip_kind: IpAddrKind) {
+        println!("ip_kind: {:?}", ip_kind);
+    }
+    router(ipV4);
+    router(ipV6);
 
-    user2.active = false;
-
-    let user3 = User {
-        active: false,
-        ..user2
-    };
-
-    let customWidth = 10;
-
-    let mut rectangle = Rectangle {
-        width: dbg!(10 * customWidth),
-        height: 20,
-    };
-
-    println!("area: {}", area(&rectangle));
-    println!("rectangle: {:?}", rectangle);
-
-    dbg!(&rectangle);
-
-    println!("area: {}", rectangle.area());
-    rectangle.inc_width(200);
-    println!("area: {}", rectangle.area());
-
-    let square = Rectangle::square(10);
-    println!("square: {:?}", square);
-
-    let isRightWidth = Rectangle::isRightWidth(10);
-    println!("isRightWidth: {}", isRightWidth);
-}
-
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.height * self.width
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
     }
 
-    fn inc_width(&mut self, delta: u32) {
-        self.width += delta;
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    enum IpAddr2 {
+        V4(String),
+        V6(String),
     }
 
-    fn square(size: u32) -> Rectangle {
-        Rectangle {
-            width: size,
-            height: size,
+    let home2 = IpAddr2::V4(String::from("127.0.0.1"));
+    let loopback2 = IpAddr2::V6(String::from("::1"));
+
+    enum IpAddr3 {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let home3 = IpAddr3::V4(127, 0, 0, 1);
+    let loopback3 = IpAddr3::V6(String::from("::1"));
+
+    struct Ipv4Addr {
+        // --snip--
+    }
+
+    struct Ipv6Addr {
+        // --snip--
+    }
+
+    enum IpAddr4 {
+        V4(Ipv4Addr),
+        V6(Ipv6Addr),
+    }
+
+    #[derive(Debug)]
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    impl Message {
+        fn call(&self) {
+            // method body would be defined here
         }
     }
 
-    fn isRightWidth(width: u32) -> bool {
-        width > 5
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let my_move = Message::Move { x: 10, y: 20 };
+    println!("my_move: {:?}", my_move);
+
+    struct QuitMessage; // unit struct
+    struct MoveMessage {
+        x: i32,
+        y: i32,
+    }
+    struct WriteMessage(String); // tuple struct
+    struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
+    // impl MessageImpl for QuitMessage {
+    //     fn call(&self) {
+    //         println!("QuitMessage");
+    //     }
+    // }
+
+    // impl MessageImpl for MoveMessage {
+    //     fn call(&self) {
+    //         println!("MoveMessage");
+    //     }
+
+    //     fn move_message(&self) {
+    //         println!("MoveMessage");
+    //     }
+    // }
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    // let sum = x + y;
+
+    let sum = x + y.unwrap();
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
+    }
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => {
+                println!("Lucky penny!");
+                1
+            }
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+
+    let coin = Coin::Penny;
+    println!("coin: {}", value_in_cents(coin));
+
+    #[derive(Debug)]
+    enum UsState {
+        Alabama,
+        Alaska,
+    }
+
+    enum Coin2 {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState),
+    }
+
+    fn value_in_cents2(coin: Coin2) -> u8 {
+        match coin {
+            Coin2::Penny => {
+                println!("Lucky penny!");
+                1
+            }
+            Coin2::Nickel => 5,
+            Coin2::Dime => 10,
+            Coin2::Quarter(state) => {
+                println!("State quarter from {:?}!", state);
+                25
+            }
+        }
+    }
+
+    let coin = Coin2::Quarter(UsState::Alaska);
+    println!("coin: {}", value_in_cents2(coin));
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("five: {:?}", five);
+
+    fn add_fancy_hat() {}
+    fn remove_fancy_hat() {}
+    fn move_player(num_spaces: u8) {}
+
+    let dice_roll = 9;
+    match dice_roll {
+        1 => add_fancy_hat(),
+        2 => remove_fancy_hat(),
+        3 => move_player(4),
+        4 => move_player(3),
+        5 => {
+            add_fancy_hat();
+            move_player(3);
+        }
+        6 => {
+            remove_fancy_hat();
+            move_player(2);
+        }
+        _ => (),
+    }
+
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("Max is {}", max),
+        None => println!("No max provided"),
+    }
+
+    match config_max {
+        Some(max) => println!("Max is {}", max),
+        _ => println!("No max provided"),
+    }
+
+    if let Some(max) = config_max {
+        println!("Max is {}", max);
+    } else {
+        println!("No max provided");
     }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//         self.height * self.width
+//     }
+
+//     fn inc_width(&mut self, delta: u32) {
+//         self.width += delta;
+//     }
+
+//     fn square(size: u32) -> Rectangle {
+//         Rectangle {
+//             width: size,
+//             height: size,
+//         }
+//     }
+
+//     fn isRightWidth(width: u32) -> bool {
+//         width > 5
+//     }
+// }
+
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
 
 // fn is_child(age: i32) -> bool {
 //     if (age < 18) {
