874a85d686689d2d80ae792ef79974567e7448b5
Novar <xuwab77@gmail.com>
2023-10-18 11:19:38 +0800

    string

--- src/main.rs
+++ src/main.rs
@@ -482,68 +482,105 @@ fn main() {
     // back_of_house::fix_incorrect_order();
 
     //vector
-    let v: Vec<i32> = Vec::new();
-    println!("v: {:?}", v);
+    // let v: Vec<i32> = Vec::new();
+    // println!("v: {:?}", v);
 
-    let v = vec![1, 2, 3];
-    println!("v: {:?}", v);
+    // let v = vec![1, 2, 3];
+    // println!("v: {:?}", v);
 
-    let mut v2: Vec<i32> = Vec::new();
-    v2.push(5);
-    v2.push(6);
-    v2.push(7);
-    println!("v2: {:?}", v2);
+    // let mut v2: Vec<i32> = Vec::new();
+    // v2.push(5);
+    // v2.push(6);
+    // v2.push(7);
+    // println!("v2: {:?}", v2);
 
-    let v3 = vec![1, 2, 3, 4, 5];
-    let third: &i32 = &v3[2];
-    println!("third: {}", third);
+    // let v3 = vec![1, 2, 3, 4, 5];
+    // let third: &i32 = &v3[2];
+    // println!("third: {}", third);
 
-    let third: Option<&i32> = v.get(2);
-    println!("third: {:?}", third);
-    match third {
-        Some(third) => println!("third: {}", third),
-        None => println!("third: None"),
+    // let third: Option<&i32> = v.get(2);
+    // println!("third: {:?}", third);
+    // match third {
+    //     Some(third) => println!("third: {}", third),
+    //     None => println!("third: None"),
+    // }
+
+    // let v4 = vec![1, 2, 3, 4, 5];
+    // // let does_not_exist = &v4[100];
+    // // println!("does_not_exist: {}", does_not_exist);
+    // let does_not_exist = v.get(100);
+    // println!("does_not_exist: {:?}", does_not_exist);
+
+    // let mut v5 = vec![1, 2, 3, 4, 5];
+    // let first = &v5[0];
+    // // v5.push(6);
+    // println!("first: {}", first);
+
+    // let v6 = vec![100, 32, 57];
+    // for i in &v6 {
+    //     println!("{}", i);
+    // }
+
+    // let mut v7 = vec![100, 32, 57];
+    // for i in &mut v7 {
+    //     *i += 50;
+    // }
+    // println!("v7: {:?}", v7);
+
+    // enum SpreadsheetCell {
+    //     Int(i32),
+    //     Float(f64),
+    //     Text(String),
+    // }
+
+    // let row = vec![
+    //     SpreadsheetCell::Int(3),
+    //     SpreadsheetCell::Float(10.12),
+    //     SpreadsheetCell::Text(String::from("blue")),
+    // ];
+
+    // {
+    //     let v8 = vec![1, 2, 3, 4];
+    //     // do stuff with v
+    // }// <- v goes out of scope and is freed here
+
+
+    //String
+    let mut s = String::new();
+    let data = "initial contents";
+    let s = data.to_string();
+    let s = "initial contents".to_string();
+    let s = String::from("initial contents");
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    println!("s: {}", s);
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    s1.push('s');
+    println!("s1: {}", s1);
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+    println!("s3: {}", s3);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    // let s = s1 + "-" + &s2 + "-" + &s3;
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("s: {}", s);
+
+    let hello = "Здравствуйте";
+    let s = &hello[0..4];
+    println!("s: {}", s);
+
+    for c in hello.chars() {
+        println!("{}", c);
     }
-
-    let v4 = vec![1, 2, 3, 4, 5];
-    // let does_not_exist = &v4[100];
-    // println!("does_not_exist: {}", does_not_exist);
-    let does_not_exist = v.get(100);
-    println!("does_not_exist: {:?}", does_not_exist);
-
-    let mut v5 = vec![1, 2, 3, 4, 5];
-    let first = &v5[0];
-    // v5.push(6);
-    println!("first: {}", first);
-
-    let v6 = vec![100, 32, 57];
-    for i in &v6 {
-        println!("{}", i);
-    }
-
-    let mut v7 = vec![100, 32, 57];
-    for i in &mut v7 {
-        *i += 50;
-    }
-    println!("v7: {:?}", v7);
-
-    enum SpreadsheetCell {
-        Int(i32),
-        Float(f64),
-        Text(String),
-    }
-
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Float(10.12),
-        SpreadsheetCell::Text(String::from("blue")),
-    ];
-
-    {
-        let v8 = vec![1, 2, 3, 4];
-        // do stuff with v
-    }// <- v goes out of scope and is freed here
-
     
 }
 
